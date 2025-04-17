7d06f3c12f26a8d3b33f8a684c611a246583567c
Rizwan Ali <rizmahar088@gmail.com>
2025-04-17 17:24:51 +0500

    fix height of videos

--- src/components/documenrySindh/document.jsx
+++ src/components/documenrySindh/document.jsx
@@ -7,14 +7,14 @@ function Documentry() {
         {DocumentrySindh.map((item_card) => {
           return (
             <div
-              className="col-lg-3 col-md-4 col-sm-12 mt-2 CardBox"
+              className="col-lg-3 col-md-4 col-sm-12 mt-2 mb-2 CardBox"
               key={item_card.id}
             >
               <div
-                className="documentry mt-2 w-100 "
+                className="documentry mt-2"
                 dangerouslySetInnerHTML={{ __html: item_card.video }}
               ></div>
-              <div className="card-body text pt-1 px-1 mb-2">
+              <div className="card-body text pt-1 mb-3 mx-3">
                 <p className="card-text">{item_card.title}</p>
               </div>
             </div>
